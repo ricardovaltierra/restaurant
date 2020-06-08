363d0f4c3d15a093dce5b7d5b2429f758fb2f314
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-06-08 17:28:29 -0500

    log test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
 
 function component() {
-  const element = document.createElement('div');
+  // const element = document.createElement('div');
+
+  const element = console.log("This is a test");
 
   // Lodash now imported
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
 
-document.body.appendChild(component());
+component();
+// document.body.appendChild(component());
