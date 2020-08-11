74792ea56aea82ff84f52b3a17e4de813e3e13d9
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-08-11 17:18:24 -0500

    added preview image

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,8 @@ module.exports = {
       filename: 'index.html',
       meta: {
         author: 'Ricardo Valtierra',
-        'og:image': { property: 'og:image', content: 'preview.png' },
-        'linkedin:image': { name: 'linkedin:image', content: 'preview.png' },
+        'og:image': { property: 'og:image', content: 'usage_1.gif' },
+        'linkedin:image': { name: 'linkedin:image', content: 'usage_1.gif' },
         'og:url': { property: 'og:url', content: 'https://restaurant-clone.netlify.app/' },
         'linkedin:card': 'summary',
         'og:description': { property: 'og:description', content: 'This is a tribute page from the official website \'donpastor.mx\'.' },
